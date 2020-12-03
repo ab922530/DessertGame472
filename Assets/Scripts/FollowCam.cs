8212667bf18696e84f34fb6a0dfea0942dbbf037
Austin Bramley <ab922530@wcupa.edu>
2020-12-03 13:45:53 -0500

    basemovement

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraFollow();
+    }
+    void cameraFollow()
+    {
+        float charPosX = transform.position.x;
+        float charPosZ = transform.position.z - 15f;
+        float cameraOffset = -4;
+
+        Camera.main.transform.position = new Vector3(charPosX, cameraOffset, charPosZ);
+
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    Rigidbody rb;
+    public float runSpeed = 10f;
+    public float jumpPower = 10f;
+
+    public Vector3 VelocityRight;
+    public Vector3 VelocityLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Movement");
+        rb = GetComponent<Rigidbody>();
+        VelocityRight = new Vector3(runSpeed, 0, 0);
+        VelocityLeft = new Vector3(-runSpeed, 0, 0);
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        run();
+    }
+
+    void run()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Right");
+            rb.MovePosition(rb.position + VelocityRight * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("LEft");
+            rb.MovePosition(rb.position + VelocityLeft * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+        }
+    }
+}
