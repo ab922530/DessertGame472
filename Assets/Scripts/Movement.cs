df5b0d678eac58bf54dd8a15426351c9a4956aa4
Austin Bramley <ab922530@wcupa.edu>
2020-12-03 14:40:58 -0500

    leg movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,11 +6,16 @@ public class Movement : MonoBehaviour
 {
     [Header("Set in Inspector")]
     Rigidbody rb;
+    Rigidbody rbR;
+    Rigidbody rbL;
+    public GameObject legR;
+    public GameObject legL;
     public float runSpeed = 10f;
-    public float jumpPower = 10f;
+    public float jumpPower = 20f;
 
     public Vector3 VelocityRight;
     public Vector3 VelocityLeft;
+    public Vector3 VelocityUp;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +24,16 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         VelocityRight = new Vector3(runSpeed, 0, 0);
         VelocityLeft = new Vector3(-runSpeed, 0, 0);
-       
+        VelocityUp = new Vector3(0, jumpPower, 0);
+        legR = this.gameObject.transform.GetChild(2).GetChild(0).gameObject;
+        legL = this.gameObject.transform.GetChild(2).GetChild(1).gameObject;
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+       // legR.transform.rotation = Quaternion.Euler(0, 0, 45);
         run();
     }
 
@@ -34,15 +43,29 @@ public class Movement : MonoBehaviour
         {
             Debug.Log("Right");
             rb.MovePosition(rb.position + VelocityRight * Time.fixedDeltaTime);
+
+            legR.transform.rotation = Quaternion.Euler(0, 0, 45);
+            legL.transform.rotation = Quaternion.Euler(0, 0, -45);
+            if (legR.transform.eulerAngles.z == -45)
+            {
+                legR.transform.rotation = Quaternion.Euler(0, 0, 45);
+                legL.transform.rotation = Quaternion.Euler(0, 0, -45);
+            }
+            if (legR.transform.eulerAngles.z == 45)
+            {
+                legR.transform.rotation = Quaternion.Euler(0, 0, -45);
+                legL.transform.rotation = Quaternion.Euler(0, 0, 45);
+            }
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("LEft");
             rb.MovePosition(rb.position + VelocityLeft * Time.fixedDeltaTime);
         }
-
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
+            rb.MovePosition(rb.position + VelocityUp * Time.fixedDeltaTime);
         }
     }
 }
