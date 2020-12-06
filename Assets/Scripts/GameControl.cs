51cd5b84e04489345f93525a6de209d70a297ec9
John-Vincent Saddic <js873514@wcupa.edu>
2020-12-05 22:27:32 -0500

    Some UI stuff

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -8,9 +8,9 @@ public class GameControl : MonoBehaviour
 
     [Header("set in insepctor")]
 
-    public Text uitLevel;  // The UIText_Level Text
-    public Text uitPoints;  // The UIText_Shots Text
-    public Text uitHighScore; // The UIText_HighScore Text
+    //public Text uitLevel;  // The UIText_Level Text
+    //public Text uitPoints;  // The UIText_Points Text
+    //public Text uitHighScore; // The UIText_HighScore Text
     public GameObject[] tables;   // An array of the levels
 
 
