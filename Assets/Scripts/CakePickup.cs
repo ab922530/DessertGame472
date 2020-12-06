51894e8aaaf6c128e73c0a243972a3e6bbab88a5
Austin Bramley <ab922530@wcupa.edu>
2020-12-05 21:08:04 -0500

    spinning cake

---
+++ Assets/Scripts/CakePickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakePickup : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 angleturn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        angleturn = new Vector3(0, 360, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Quaternion deltaRot = Quaternion.Euler(angleturn * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRot);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "chunkyKid")
+        {
+            Debug.Log("hit");
+            Destroy(this.gameObject);
+        }
+
+    }
+}
