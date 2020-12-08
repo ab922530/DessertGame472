93406ecabdf52534a250fb0ee5317942a4aef866
Austin Bramley <ab922530@wcupa.edu>
2020-12-08 14:00:17 -0500

    final boss

---
+++ Assets/Scripts/BossMinion.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMinion : MonoBehaviour
+{
+    public Vector3 Velocity;
+    public Vector3 StartPos;
+    public float speed = .1f;
+    public GameObject OuchBox;
+    public float deathDuration = 5.0f;
+
+    public Vector3 StartScale;
+    public Vector3 DeadScale;
+    bool dead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dead = false;   
+        this.Velocity = new Vector3(-speed, 0, 0);
+        this.transform.position = new Vector3(65.9f, -9.4f, -9.6f);
+        StartPos = this.transform.position;
+        StartScale = this.transform.localScale;
+        DeadScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, 1);
+        OuchBox = this.gameObject.transform.GetChild(2).gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x > -5)
+        {
+            this.transform.Translate(Velocity * speed * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(this);
+        }
+        checkDeath();
+    }
+
+    public void die()
+    {    
+        Destroy(OuchBox);
+        this.dead = true;
+    }
+
+    public void checkDeath()
+    {
+        if (dead == true)
+        {
+            float timer = 0.0f;
+            while (timer < deathDuration)
+            {
+                timer += Time.deltaTime;
+                float t = timer / deathDuration;
+                t = t * t * t * (t * (6f * t - 15f) + 10f);
+                this.transform.localScale = Vector3.Lerp(StartScale, DeadScale, t);
+
+            }
+
+            Invoke("killVeggie", .1f);
+        }
+    }
+
+    public void killVeggie()
+    {
+
+        dead = true;
+        foreach (Transform child in this.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        this.transform.position = new Vector3(1000, 1000, 1000);
+    }
+
+}
---
+++ Assets/Scripts/MinionHitbox.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionHitbox : MonoBehaviour
+{
+    BossMinion pscript;
+    public BossScript bScript;
+    public GameObject bigboi;
+    int touched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        touched = 0;
+        bScript = bigboi.GetComponent<BossScript>();
+        pscript = transform.parent.gameObject.GetComponent<BossMinion>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "chunkyKid")
+        {
+           
+            if (touched == 0)
+            {
+                touched = 1;
+                bScript.Invoke("hurt", 0f);
+                pscript.Invoke("die", 0f);
+            }
+        }
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (touched == 1)
+        {
+            touched = 0;
+        }
+    }
+
+
+
+}
